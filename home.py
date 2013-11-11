dd82ee367234ec52cede873ef976ba9ef98e822e
giulioungaretti <giulio.ungaretti@gmail.com>
2013-11-11 01:47:07 +0100

    wokring example

--- home.py
+++ home.py
@@ -3,6 +3,8 @@
 import web
 import markdown
 import os 
+import inspect
+import glob
 
 __version__ = '0.01beta' 
 __author__  = 'Giulio, Giulio.ungaretti@gmail.com' 
@@ -10,8 +12,8 @@ __author__  = 'Giulio, Giulio.ungaretti@gmail.com'
 # define the urls of the web page
 
 urls =  (
-    # '/', 'index'  #matches the / regex to the  class index
-    '/(.*)', 'page',
+ #   '/^(?!index).*', 'page',  #matches the / regex to the  class index
+    '/(.*)', 'index',
     )
 
 # templates are found in the templates directory
@@ -23,12 +25,13 @@ md = markdown.Markdown(output_format='html4')
 
 # define classes for the website
 
-class index:
-    '''
-    class to define the index page of my web site
-    '''
-    def GET(self):
-        return "hello Giulio"
+# class index:
+#     '''
+#     class to define the index page of my web site
+#     '''
+#     def GET(self):
+#         return "hello Giulio "
+
 
 
 class page:
@@ -36,23 +39,40 @@ class page:
     class to define the other pages of  my web site
     '''
     def GET(self,url):
-        if url == "" or url.endswith("/"):
-            url += "index"
-        print url
         # Each URL maps to the corresponding .txt file in pages/
-        page_file = 'pages/%s.md' %(url)     
+        # page_file = 'pages/%s.md' %(url)     
+        print url
+        # # Try to open the text file, returning a 404 upon failure
 
-        # Try to open the text file, returning a 404 upon failure
+
+        # # Read the entire file, converting Markdown content to HTML
+        # content = f.read()
+        # content = md.convert(content)
+
+        # # Render the page.html template using the converted content
+        return render.page(url)
+
+class index:
+  def GET(self,url):
+    home = expanduser("~")
+    if url == "index": 
+        all_f = [] 
+        for dirname,dirpath,filez in os.walk('/home/giulio/Dropbox/Documents/'):
+            for i in filez:
+                if i[-3:] == '.md':
+                    all_f.append([ dirname,i])
+        return render.list(all_f)  
+    else:
+        
+        file_name = url.split('/')[-1]
+        folder_path = '/'.join(url.split('/')[:-1])
+        total = '/'.join([folder_path,file_name])
         try:
-            f = open(page_file, 'r')
+            f = open('/'+total, 'r')
+            content = f.read()
+            content = md.convert(content)
         except IOError:
             return web.notfound()
-
-        # Read the entire file, converting Markdown content to HTML
-        content = f.read()
-        content = md.convert(content)
-
-        # Render the page.html template using the converted content
         return render.page(content)
 
 
@@ -60,5 +80,3 @@ class page:
 if __name__ == "__main__":
     app = web.application(urls,globals())
     app.run()
-
-
